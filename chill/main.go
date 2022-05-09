cedf12420e2564f5c71d32576a3f3674d5cc16b2
Marisame <rulivas26@gmail.com>
2022-05-09 15:49:35 -0500

    Adding a Chill dir

---
+++ chill/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var a int
+
+type hotdog int
+
+var b hotdog
+
+func main() {
+	a = 42
+	b = 43
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+	a = int(b)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+}
