5f51f42766eddf49364efbd41deb5dcf237531ad
Marisame <rulivas26@gmail.com>
2022-05-12 16:48:45 -0500

    application

---
+++ application/custom-sort/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	First string
+	Age   int
+}
+
+type ByAge []Person
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+type ByName []Person
+
+func (bn ByName) Len() int           { return len(bn) }
+func (bn ByName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
+func (bn ByName) Less(i, j int) bool { return bn[i].First < bn[j].First }
+
+func main() {
+	p1 := Person{"James", 32}
+	p2 := Person{"Moneypenny", 27}
+	p3 := Person{"Q", 64}
+	p4 := Person{"M", 56}
+
+	people := []Person{p1, p2, p3, p4}
+
+	fmt.Println(people)
+	sort.Sort(ByAge(people))
+	fmt.Println("by age:", people)
+	sort.Sort(ByName(people))
+	fmt.Println("by name:", people)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Marisame254/golang_course
+
+go 1.18
+
+require golang.org/x/crypto v0.0.0-20220511200225-c6db032c6c88
