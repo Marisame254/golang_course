d2297c997df59f4c7b61c74cc63059ecd944c934
Marisame <rulivas26@gmail.com>
2022-05-18 10:29:47 -0500

    Better mutex example

---
+++ concurrency/mutex/main.go
@@ -0,0 +1,36 @@
+// user go run -race reace-condition/main.go (command line)
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+var counter int
+var mutex sync.Mutex
+
+func main() {
+	wg.Add(2)
+
+	go incrementor("Foo")
+	go incrementor("Bar")
+
+	wg.Wait()
+
+	fmt.Println("Final Counter:", counter)
+}
+
+func incrementor(s string) {
+	for i := 0; i < 20; i++ {
+		time.Sleep(time.Duration(rand.Intn(20)) * time.Millisecond)
+		mutex.Lock()
+		counter++
+		fmt.Println(s, i, "Counter", counter)
+		mutex.Unlock()
+	}
+	wg.Done()
+}
